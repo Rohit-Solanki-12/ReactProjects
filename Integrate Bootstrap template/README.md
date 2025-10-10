34ce92750ba447d674ebafa186b918559ae0ef4f
Rohit-Solanki-12 <rohitkssolanki2005@gmail.com>
2025-10-10 10:55:39 +0530

    Bootstrap project add

---
+++ Integrate Bootstrap template/README.md
@@ -0,0 +1,4 @@
+I have mentioned two steps for integrating bootstrap in my project
+
+1. Is implementing link at the index.html
+2. Is by Installing the packages at the projects
---
+++ Integrate Bootstrap template/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-center text-primary">Hello, Bootstrap!</h1>
+        <button className="btn btn-success">Click Me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
